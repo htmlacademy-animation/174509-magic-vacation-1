990712813d6be57e330a99886a8399403f32d26b
Inna <inna.jobmail@gmail.com>
2024-03-12 16:24:17 +0300

    add animation for rule page

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -2,36 +2,12 @@ export default () => {
 
   document.addEventListener(`DOMContentLoaded`, function () {
 
+    const body = document.querySelector(`.js-body`);
+    body.classList.add(`loaded`);
+
     // intro
     const introMessage = document.querySelector(`.js-intro-message`);
     introMessage.classList.add(`intro__message--show`);
-
-    // game
-    const gameScreen = document.getElementById(`game`);
-    const formField = document.querySelector(`.js-form-field`);
-
-    function checkActiveClass() {
-      if (gameScreen.classList.contains(`active`)) {
-        formField.classList.add(`form__field--show`);
-      } else {
-        formField.classList.remove(`form__field--show`);
-      }
-    }
-
-    const observer = new MutationObserver(function (mutationsList) {
-      for (let mutation of mutationsList) {
-        if (mutation.type === `attributes` && mutation.attributeName === `class`) {
-          checkActiveClass();
-        }
-      }
-    });
-
-    observer.observe(gameScreen, {attributes: true});
-
-    checkActiveClass();
-
-    const body = document.querySelector(`.js-body`);
-    body.classList.add(`loaded`);
 
     // overlap
     const overlap = document.querySelector(`.overlap`);
@@ -54,6 +30,5 @@ export default () => {
     addOverlap(prizes);
 
   });
-
 
 };
