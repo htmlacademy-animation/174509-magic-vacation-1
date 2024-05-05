dd00520354dda8dd670f5b7b796289a86305deca
Inna <inna.jobmail@gmail.com>
2024-05-05 20:51:11 +0300

    add module2-task2

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -5,9 +5,58 @@ export default () => {
     const body = document.querySelector(`.js-body`);
     body.classList.add(`loaded`);
 
-    // intro
-    const introMessage = document.querySelector(`.js-intro-message`);
-    introMessage.classList.add(`intro__message--show`);
+    // title animation
+    class SplitText {
+      constructor(
+          elementSelector
+      ) {
+        this._elementSelector = elementSelector;
+        this._element = document.querySelector(this._elementSelector);
+        this.prePareText();
+      }
+
+      createElement(letter) {
+        const span = document.createElement(`span`);
+        span.textContent = letter;
+        return span;
+      }
+
+      prePareText() {
+        if (!this._element) {
+          return;
+        }
+        const text = this._element.textContent.trim().split(` `).filter((line) => line !== ``);
+
+        const content = text.reduce((fragmentParent, word) => {
+          const wordElement = Array.from(word).reduce((fragment, line) => {
+            fragment.appendChild(this.createElement(line));
+            return fragment;
+          }, document.createDocumentFragment());
+          const wordContainer = document.createElement(`span`);
+          wordContainer.classList.add(`split__words`);
+          wordContainer.appendChild(wordElement);
+          fragmentParent.appendChild(wordContainer);
+          return fragmentParent;
+        }, document.createDocumentFragment());
+
+        this._element.innerHTML = ``;
+        this._element.appendChild(content);
+      }
+
+    }
+
+    const targetsForSplit = [
+      `.intro__title`,
+      `.intro__date`,
+      `.slider__item-title`,
+      `.prizes__title`,
+      `.rules__title`,
+      `.game__title`,
+    ];
+
+    for (let target of targetsForSplit) {
+      new SplitText(target);
+    }
 
     // overlap
     const overlap = document.querySelector(`.overlap`);
@@ -17,7 +66,7 @@ export default () => {
       link.addEventListener(`click`, (event) => {
         event.preventDefault();
         overlap.classList.add(`overlap--show`);
-        setTimeout(onAnimationComplete, 2000);
+        setTimeout(onAnimationComplete, 600);
 
       });
 
